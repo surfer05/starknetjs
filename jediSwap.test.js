719c43b8d031968bb3f05e7a2c2efd058eedace6
surfer05 <rahul05alok@gmail.com>
2023-06-25 19:37:27 +0530

    Latest commit

--- jediSwap.test.js
+++ jediSwap.test.js
@@ -1,12 +1,35 @@
+// Imports
+const { Provider, Contract, json, Account, ec, Signer } = require("starknet");
+const dotenv = require("dotenv");
+const { Signature } = require("ethers");
+
 // Initialize Provider
-import { Provider } from "starknet";
-const provider = new Provider({sequencer :{network:'goerli-alpha'}})
+const provider = new Provider({ sequencer: { baseUrl: "https://alpha4-2.starknet.io"} })
 
-// initialize existing pre-deployed account 0 of devnet
-const privateKey = "0xe3e70682c2094cac629f6fbed82c07cd"
+// Connecting the deployed JediSwap contract in Goerli Testnet
+const testAddress = "0x02bcc885342ebbcbcd170ae6cafa8a4bed22bb993479f49806e72d96af94c965"
+const blockIdentifier = 361046
+const { abi: testAbi } = provider.getClassAt(testAddress, blockIdentifier);
+if (testAbi === undefined) { throw new Error("no abi.") };
+const myTestContract = new Contract(testAbi, testAddress, provider)
+
+// Connect the account 
+const privateKey = process.env.OZ_NEW_ACCOUNT_PRIVKEY;
 const starkKeyPair = ec.getKeyPair(privateKey);
-const accountAddress = "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a"
-
+const accountAddress = "0x05fB39C55A5Ea0B757CC60790292b19F4B3a2620ab90A6049900b28ad7B59Cca"
 const account = new Account(provider, accountAddress, starkKeyPair);
 
-import { Account, ec , Provider } from "starknet";
+// Connect account with the contract
+myTestContract.connect(account)
+
+// Interaction with the contract with call
+// describe('Jedi Swap Test Suite' , () =>{
+//     let starknetSigner;
+
+//     beforeEach(async () =>{
+//         starknetSigner = new Signer({sequencer: {network: 'goerli-alpha'}})
+
+//     })
+// })
+
+
