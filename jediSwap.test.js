56866583522e3e64526940e9ee752712e805cc9d
surfer05 <rahul05alok@gmail.com>
2023-06-26 16:59:18 +0530

    Fetch ABI successful

--- jediSwap.test.js
+++ jediSwap.test.js
@@ -1,20 +1,18 @@
 // Imports
-const { Provider, Contract, json, Account, ec, Signer } = require("starknet");
+const { Provider, Contract, json, Account, ec, Signer, constants } = require("starknet");
 const { ethers } = require("ethers")
 const dotenv = require("dotenv");
 require("dotenv").config({ path: ".env" });
 const { Signature } = require("ethers");
 const PRIVATE_KEY = process.env.OZ_NEW_ACCOUNT_PRIVKEY;
 
-async function connectContract() {
-
-}
 
 describe('Jedi Swap test suite', () => {
-
+    let provider;
+    let myTestContract;
     beforeEach(async () => {
         // Initialize Provider
-        const provider = new Provider({ sequencer: { baseUrl: "https://alpha4-1.starknet.io" } })
+        const provider = new Provider({ sequencer: { network: constants.NetworkName.SN_GOERLI } }) // for testnet 1
 
         // Connect the account 
         const privateKey = PRIVATE_KEY;
@@ -25,24 +23,26 @@ describe('Jedi Swap test suite', () => {
         // console.log("Account", account)
 
         // Connecting the deployed JediSwap contract in Goerli Testnet
-
         const testAddress = "0x02bcc885342ebbcbcd170ae6cafa8a4bed22bb993479f49806e72d96af94c965"
-        const { abi: testAbi } = await provider.getClassAt(testAddress);
-        if (testAbi === undefined) { throw new Error("no abi.") };
-        const myTestContract = new Contract(testAbi, testAddress, provider);
-        // const blockIdentifier = 361046
-        // const testAbi = await provider.getClassAt(testAddress, blockIdentifier);
-        // console.log(abi)
-        // if (testAbi === undefined) { throw new Error("no abi.") };
-        // const myTestContract = new Contract(testAbi, testAddress, provider)
+        const contractClass = await provider.getClassAt(testAddress, 361046);
+        if (contractClass.abi === undefined) { throw new Error("no abi.") };
+        console.log(contractClass.abi)
+        myTestContract = new Contract(contractClass.abi, testAddress, provider);
+
 
         // Connect account with the contract
         myTestContract.connect(account)
     })
 
     it("should return the admin address", async function () {
-        await myTestContract.call(get_admin());
-
+        const admin = await myTestContract.get_admin();
+        console.log(admin)
     })
 
 })
+
+ // const blockIdentifier = 361046
+        // const testAbi = await provider.getClassAt(testAddress, blockIdentifier);
+        // console.log(abi)
+        // if (testAbi === undefined) { throw new Error("no abi.") };
+        // const myTestContract = new Contract(testAbi, testAddress, provider)
